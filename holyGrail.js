f2eea7fc536e4325932787213e295a78d294ec31
Musa <79163385+musayuksel@users.noreply.github.com>
2022-11-09 22:59:20 +0000

    Add boot count and findTotalChestValue

--- holyGrail.js
+++ holyGrail.js
@@ -1,17 +1,19 @@
 const allData = {
-  recursiveCount: 0,
   'Dead spiders': 0,
   'Total chest value': 0,
+  bootSizes: {},
 };
+let recursiveCount= 0;
 
 async function holyGrail(jsonLink) {
   const response = await fetch(jsonLink);
   const data = await response.json();
   spreadObject(data);
+  
 }
 
 function spreadObject(objectData) {
-  allData.recursiveCount++;
+  recursiveCount++;
   Object.entries(objectData).forEach(([key, value]) => {
     if (objectData?.contents?.hasOwnProperty('holy-grail')) {
       allData['Holy Grail location'] = objectData.location;
@@ -20,19 +22,22 @@ function spreadObject(objectData) {
       countDeadSpiders(value);
     } else if (key === 'sapphire' || key === 'ruby' || key === 'diamond') {
       findTotalChestValue(key, value);
+    } else if (key === 'boots') {
+      countBootSizes(value);
     } else if (typeof value === 'object') {
-      spreadObject(value);
+      return spreadObject(value);
     } else if (typeof value === 'string' && value.includes('http')) {
       const [first] = value.split('json');
       const [_, end] = first.split('https://');
       const newLink = 'https://' + end + 'json';
-      holyGrail(newLink);
+     return holyGrail(newLink);
     }
   });
-  allData.recursiveCount--;
-  if (allData.recursiveCount === 0) {
+  recursiveCount--;
+  if (recursiveCount === 0) {
     returnHolyGrailLocation();
   }
+ 
 }
 
 function countDeadSpiders(spider) {
@@ -41,14 +46,6 @@ function countDeadSpiders(spider) {
   }
 }
 
-holyGrail(
-  'https://e0f5e8673c64491d8cce34f5.z35.web.core.windows.net/treasure.json'
-);
-
-function returnHolyGrailLocation() {
-  console.log({ allData });
-  return allData.holyGrailLocation;
-}
 
 function findTotalChestValue(key, value) {
   if (key === 'sapphire') {
@@ -59,6 +56,28 @@ function findTotalChestValue(key, value) {
     allData['Total chest value'] += value.count * 400;
   }
 }
+
+function countBootSizes(boots) {
+  if (boots.size in allData.bootSizes) {
+    allData.bootSizes[boots.size]++;
+  } else {
+    allData.bootSizes[boots.size] = 1;
+  }
+}
+
+function returnHolyGrailLocation() {
+  console.log("recursiveCount", recursiveCount);
+  allData['Most common boot size']=Object.entries(allData.bootSizes).sort((a, b) => b[1] - a[1])[0][0];//find the most common boot size
+  // const formattedDoubloon =allData['Total chest value'] + " doubloons";
+  // allData['Total chest value']=formattedDoubloon ;//format the total chest value
+  console.log({ allData });
+  return allData.holyGrailLocation;
+}
+
+holyGrail(
+  'https://e0f5e8673c64491d8cce34f5.z35.web.core.windows.net/treasure.json'
+);
+
 // Holy Grail location: 20.19 -19.83
 // Total chest value: 25600 doubloons
 // Dead spiders: 27
